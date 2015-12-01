0808cf2af0506c0ce87e883c484de68ffb964b3b
root <root@sotonwo.sotonwo.d9.internal.cloudapp.net>
2015-12-01 15:01:17 +0000

    Updated Map to include Heapmaps

--- js/historic.js
+++ js/historic.js
@@ -25,23 +25,98 @@ function printObject(o) {
 }
 
 
-function getNeighbourhoods() {
-
-    //var results = ['ALDERSHOT-NORTH.kml','ALDERSHOT-SOUTH.kml','ALTON-RURAL.kml','ALTON.kml','ANDOVER-EAST.kml','ANDOVER-NORTH.kml','ANDOVER-SOUTH.kml','ANDOVER-TOWN.kml','BARGATE.kml','BARNCROFT-AND-BEDHAMPTON.kml','BASINGSTOKE-CENTRE.kml','BASINGSTOKE-EAST.kml','BASINGSTOKE-NORTH.kml','BASINGSTOKE-RURAL-EAST.kml','BASINGSTOKE-RURAL-SOUTH.kml','BASINGSTOKE-RURAL-WEST.kml','BASINGSTOKE-SOUTH.kml','BASINGSTOKE-WEST.kml','BATTINS-AND-WEST-LEIGH.kml','BEVOIS.kml','BISHOPS-WALTHAM-AND-SOBERTON.kml','BITTERNE-NORTH.kml','BORDON.kml','BUTSER-AND-CLANFIELD-AND-ROWLANDS-CASTLE.kml','BUTSER_CLANFIELD_ROWLANDS-CASTLE.kml','CENTRAL-SOUTHSEA-AND-ST-JUDES.kml','CHARLES-DICKENS-AND-NELSON.kml','COSHAM.kml','COWES.kml','COXFORD-AND-REDBRIDGE-AND-MILLBROOK.kml','COXFORD_REDBRIDGE_MILLBROOK.kml','CROFTON-AND-TITCHFIELD.kml','DENMEAD-AND-SOUTHWICK.kml','EAST-COWES-AND-WOOTTON.kml','EASTLEIGH-CENTRAL.kml','EASTLEIGH-EAST.kml','EASTLEIGH-WEST.kml','EASTNEY-AND-MILTON.kml','FAREHAM.kml','FARNBOROUGH-CENTRAL.kml','FARNBOROUGH-NORTH.kml','FARNBOROUGH-SOUTH.kml','FARNBOROUGH-WEST.kml','FLEET.kml','FOUR-MARKS-AND-ROPLEY.kml','FRATTON-AND-BAFFINS.kml','GOSPORT-CENTRAL.kml','GOSPORT-EAST.kml','GOSPORT-WEST.kml','HART-RURAL-NORTH.kml','HART-RURAL-SOUTH.kml','HAVANT-AND-EMSWORTH.kml','HAYLING-ISLAND.kml','HEDGE-END-NORTH.kml','HEDGE-END-SOUTH.kml','HILSEA-AND-COPNOR.kml','HORNDEAN.kml','HYTHE.kml','LEE.kml','LISS.kml','LONGMOOR-RURAL.kml','LYMINGTON.kml','NEW-FOREST-HEART.kml','NEW-MILTON.kml','NEWPORT.kml','OWSLEBURY-AND-CURDRIDGE.kml','PAULSGROVE.kml','PEARTREE-AND-WOOLSTON.kml','PETERSFIELD.kml','PORTCHESTER.kml','PORTSWOOD.kml','RINGWOOD.kml','ROMSEY-EAST.kml','ROMSEY-NORTH.kml','ROMSEY-TOWN.kml','RYDE.kml','SANDOWN-AND-LAKE.kml','SHANKLIN-AND-VENTNOR.kml','SHIRLEY-AND-FREEMANTLE.kml','SHOLING-AND-BITTERNE.kml','ST-THOMAS.kml','TOTTON.kml','WATERLOOVILLE-NORTH.kml','WATERLOOVILLE-SOUTH.kml','WEST-MEON-AND-HAMBLEDON.kml','WESTERN-WARDS.kml','WICKHAM-AND-WHITELEY.kml','WIGHT-RURAL-EAST.kml','WIGHT-RURAL-SOUTH.kml','WIGHT-RURAL-WEST.kml','WINCHESTER-CENTRAL.kml','WINCHESTER-NORTH.kml','WINCHESTER-RURAL-EAST.kml','WINCHESTER-RURAL-NORTH.kml','WINCHESTER-RURAL-WEST.kml','WINCHESTER-SOUTH.kml','YATELEY-AND-BLACKWATER-AND-HAWLEY.kml','YATELEY_BLACKWATER-AND-HAWLEY.kml']
-    var results = ['WINCHESTER-CENTRAL.kml','WINCHESTER-NORTH.kml','WINCHESTER-RURAL-EAST.kml','WINCHESTER-RURAL-NORTH.kml','WINCHESTER-RURAL-WEST.kml','WINCHESTER-SOUTH.kml','YATELEY-AND-BLACKWATER-AND-HAWLEY.kml','YATELEY_BLACKWATER-AND-HAWLEY.kml'];
-    //var results = ['PEARTREE-AND-WOOLSTON.kml','PETERSFIELD.kml','PORTCHESTER.kml','PORTSWOOD.kml','RINGWOOD.kml','ROMSEY-EAST.kml','ROMSEY-NORTH.kml','ROMSEY-TOWN.kml','RYDE.kml','SANDOWN-AND-LAKE.kml','SHANKLIN-AND-VENTNOR.kml','SHIRLEY-AND-FREEMANTLE.kml','SHOLING-AND-BITTERNE.kml','ST-THOMAS.kml','TOTTON.kml','WATERLOOVILLE-NORTH.kml','WATERLOOVILLE-SOUTH.kml','WEST-MEON-AND-HAMBLEDON.kml','WESTERN-WARDS.kml','WICKHAM-AND-WHITELEY.kml','WIGHT-RURAL-EAST.kml','WIGHT-RURAL-SOUTH.kml','WIGHT-RURAL-WEST.kml','WINCHESTER-CENTRAL.kml','WINCHESTER-NORTH.kml','WINCHESTER-RURAL-EAST.kml','WINCHESTER-RURAL-NORTH.kml','WINCHESTER-RURAL-WEST.kml','WINCHESTER-SOUTH.kml','YATELEY-AND-BLACKWATER-AND-HAWLEY.kml','YATELEY_BLACKWATER-AND-HAWLEY.kml'];
-    return results;
-
-};
-
-
-function getDistricts(){
-   var results = ['EAST-HANTS.kml','BASINGSTOKE.kml','EASTLEIGH.kml','FAREHAM.kml','GOSPORT.kml','HART.kml','HAVANT.kml','ISLE-OF-WIGHT.kml','NEW-FOREST.kml','PORTSMOUTH.kml','RUSHMOOR.kml','SOUTHAMPTON.kml','TEST-VALLEY.kml','WINCHESTER.kml']
-   return results;
-}
 
 
 function initialize() {
+
+	var map;
+	var ajaxRequest;
+	var plotlist;
+	var plotlayers=[];
+
+
+	// // set up the map
+	map = new L.Map('map_canvas',{
+		zoom: 12
+	}
+		);
+
+	// // create the tile layer with correct attribution
+	// var osmUrl ='http://{S}tile.cloudmade.com/1a1b06b230af4efdbb989ea99e9841af/999/256/{Z}/{X}/{Y}.png'
+	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+	var osm = new L.TileLayer(
+		osmUrl, 
+		{
+			minZoom: 8, 
+			maxZoom: 18, 
+		}
+		);		
+
+	// // start the map in South-East England
+	 map.setView(new L.LatLng(22.545633, 114.040831),12);
+
+	var cfg = {
+		  // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+		  // if scaleRadius is false it will be the constant radius used in pixels
+		  "radius": 15,
+		  "maxOpacity": .8, 
+		  "blur": .75,
+		  // scales the radius based on map zoom
+		  //"scaleRadius": true, 
+		  // if set to false the heatmap uses the global maximum for colorization
+		  // if activated: uses the data maximum within the current map boundaries 
+		  //   (there will always be a red spot with useLocalExtremas true)
+		  "useLocalExtrema": true,
+		  // which field name in your data represents the latitude - default "lat"
+		  latField: 'lat',
+		  // which field name in your data represents the longitude - default "lng"
+		  lngField: 'lng',
+		  // which field name in your data represents the data value - default "value"
+		  valueField: 'count'
+	};
+
+
+	var heatmapLayer = new HeatmapOverlay(cfg);
+
+	// heatmapLayer.setDataMax(200);
+
+	var baseLayer = L.tileLayer(
+  			'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    		attribution: '...',
+    		maxZoom: 18
+  	}
+	);
+
+
+	// var testData = {
+ //  			max: 8,
+ //  			data: [{lat: 24.6408, lng:46.7728, count: 3},{lat: 50.75, lng:-1.55, count: 1}]
+	// };	
+
+	// var map = new L.Map('map_canvas', {
+	//   	center: new L.LatLng(25.6586, -80.3568),
+	//   	zoom: 4,
+	//   	layers: [osm, heatmapLayer]
+	// });
+	 map.addLayer(baseLayer);
+
+ 	map.addLayer(heatmapLayer);
+	
+	//heatmapLayer.setData(testData);
+
+
+	// var config = {
+	// 	  container: document.getElementById('map_canvas'),
+	// 	  radius: 10,
+	// 	  maxOpacity: .5,
+	// 	  minOpacity: 0,
+	// 	  blur: .75
+	// };
+	// // create heatmap with configuration
+	// var heatmapInstance = h337.create(config);
+
+
+
       //init the histogram.
       //drawHisto()
       // createLongestCascadeGraph();
@@ -63,106 +138,89 @@ function initialize() {
 
 
 
-  var myLatlng = new google.maps.LatLng(22.545633, 114.040831); //hampshire
+//   var myLatlng = new google.maps.LatLng(22.545633, 114.040831); //hampshire
   
-  var light_grey_style = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]}];
+//   var light_grey_style = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]}];
   
-  var midnight = [ { "featureType": "water", "stylers": [ { "color": "#021019" } ] }, { "featureType": "landscape", "stylers": [ { "color": "#08304b" } ] }, { "featureType": "poi", "elementType": "geometry", "stylers": [ { "color": "#0c4152" }, { "lightness": 5 } ] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [ { "color": "#0b434f" }, { "lightness": 25 } ] }, { "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "road.arterial", "elementType": "geometry.stroke", "stylers": [ { "color": "#0b3d51" }, { "lightness": 16 } ] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [ { "color": "#000000" } ] }, { "elementType": "labels.text.fill", "stylers": [ { "color": "#ffffff" } ] }, { "elementType": "labels.text.stroke", "stylers": [ { "color": "#000000" }, { "lightness": 13 } ] }, { "featureType": "transit", "stylers": [ { "color": "#146474" } ] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [ { "color": "#144b53" }, { "lightness": 14 }, { "weight": 1.4 } ] } ];
+//   var midnight = [ { "featureType": "water", "stylers": [ { "color": "#021019" } ] }, { "featureType": "landscape", "stylers": [ { "color": "#08304b" } ] }, { "featureType": "poi", "elementType": "geometry", "stylers": [ { "color": "#0c4152" }, { "lightness": 5 } ] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [ { "color": "#0b434f" }, { "lightness": 25 } ] }, { "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "road.arterial", "elementType": "geometry.stroke", "stylers": [ { "color": "#0b3d51" }, { "lightness": 16 } ] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [ { "color": "#000000" } ] }, { "elementType": "labels.text.fill", "stylers": [ { "color": "#ffffff" } ] }, { "elementType": "labels.text.stroke", "stylers": [ { "color": "#000000" }, { "lightness": 13 } ] }, { "featureType": "transit", "stylers": [ { "color": "#146474" } ] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [ { "color": "#000000" } ] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [ { "color": "#144b53" }, { "lightness": 14 }, { "weight": 1.4 } ] } ];
 
-  var myOptions = {
-    zoom: 12,
-    scrollwheel: true, 
-    center: myLatlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP,
-    mapTypeControl: false,
-    streetViewControl: false,
-    panControl: false,
-    zoomControl: true,
-    zoomControlOptions: {
-      style: google.maps.ZoomControlStyle.SMALL
-    },
-    styles: light_grey_style
-  };
+//   var myOptions = {
+//     zoom: 12,
+//     scrollwheel: true, 
+//     center: myLatlng,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP,
+//     mapTypeControl: false,
+//     streetViewControl: false,
+//     panControl: false,
+//     zoomControl: true,
+//     zoomControlOptions: {
+//       style: google.maps.ZoomControlStyle.SMALL
+//     },
+//     styles: light_grey_style
+//   };
 
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+//   var map = new google.maps.Map(document.getElementById("maps"), myOptions);
 
-  var kmlFiles = getDistricts();
 
-  infowindow = new google.maps.InfoWindow({});
+//   infowindow = new google.maps.InfoWindow({});
   
-  // myParser = new geoXML3.parser({
-  //     map: map,
-  //      infoWindow: false,
-  //      singleInfoWindow: true,
-  //      zoom: true,
-  //      markerOptions: {optimized: false},
-  //      afterParse: useTheData
-
-  // });
 
 
 
-  var heatmapPos;
 
-  var liveTweetsPos = new google.maps.MVCArray();
-    heatmapPos = new google.maps.visualization.HeatmapLayer({
-    data: liveTweetsPos,
-    maxIntensity: 100,
-    radius: 15
-  });
+//   var heatmapPos;
+
+//   var liveTweetsPos = new google.maps.MVCArray();
+//     heatmapPos = new google.maps.visualization.HeatmapLayer({
+//     data: liveTweetsPos,
+//     maxIntensity: 100,
+//     radius: 15
+//   });
     
-  // var gradient = [
-  //   'rgba(30,244,248,0)',
-  //   'rgba(30,244,248,0.5)',
-  //   'rgba(30,244,248,0.5)',
-  //   'rgba(32,248,158,0.5)',
-  //   'rgba(33,249,152,0.5)',
-  //   'rgba(51,255,75,0.5)',
-  //   'rgba(51,255,75,0.5)'
-  // ]
 
-    var gradient = [
-    'rgba(0, 255, 255, 0)',
-    'rgba(0, 255, 255, 1)',
-    'rgba(0, 191, 255, 1)',
-    'rgba(0, 127, 255, 1)',
-    'rgba(0, 63, 255, 1)',
-    'rgba(0, 0, 255, 1)',
-    'rgba(0, 0, 223, 1)',
-    'rgba(0, 0, 191, 1)',
-    'rgba(0, 0, 159, 1)',
-    'rgba(0, 0, 127, 1)',
-    'rgba(63, 0, 91, 1)',
-    'rgba(127, 0, 63, 1)',
-    'rgba(191, 0, 31, 1)',
-    'rgba(255, 0, 0, 1)'
-  ]
-  heatmapPos.set('gradient', heatmapPos.get('gradient') ? null : gradient);
-  heatmapPos.setMap(map);
+
+//     var gradient = [
+//     'rgba(0, 255, 255, 0)',
+//     'rgba(0, 255, 255, 1)',
+//     'rgba(0, 191, 255, 1)',
+//     'rgba(0, 127, 255, 1)',
+//     'rgba(0, 63, 255, 1)',
+//     'rgba(0, 0, 255, 1)',
+//     'rgba(0, 0, 223, 1)',
+//     'rgba(0, 0, 191, 1)',
+//     'rgba(0, 0, 159, 1)',
+//     'rgba(0, 0, 127, 1)',
+//     'rgba(63, 0, 91, 1)',
+//     'rgba(127, 0, 63, 1)',
+//     'rgba(191, 0, 31, 1)',
+//     'rgba(255, 0, 0, 1)'
+//   ]
+//   heatmapPos.set('gradient', heatmapPos.get('gradient') ? null : gradient);
+//   heatmapPos.setMap(map);
 
 
 
-var highlightOptions = {fillColor: "#FFFF00", strokeColor: "#000000", fillOpacity: 0.30, strokeWidth: 10};
+// var highlightOptions = {fillColor: "#FFFF00", strokeColor: "#000000", fillOpacity: 0.30, strokeWidth: 10};
 
-var normalStyle = null;
-    var polygonsAndTweets = [];
+// var normalStyle = null;
+//     var polygonsAndTweets = [];
 
-var polyTitleID = 1;
+// var polyTitleID = 1;
 
-  //shenzne boundary box location
+//   //shenzne boundary box location
 
-    var lon1 = 22.445027;
-    var lon2 = 22.861748;
-    var lat2 = 114.627296;
-    var lat1 = 113.756918;
+//     var lon1 = 22.445027;
+//     var lon2 = 22.861748;
+//     var lat2 = 114.627296;
+//     var lat1 = 113.756918;
 
 
-    ///SOCKET WORK
+//     ///SOCKET WORK
 
 
     
-    // Storage for WebSocket connections
-    var infoWindow;
+//     // Storage for WebSocket connections
+//     var infoWindow;
     //var socket = io.connect('http://socpub.cloudapp.net:9999');
 
 var socket3 = io.connect('http://sotonwo.cloudapp.net:3005');
@@ -205,17 +263,26 @@ var socket3 = io.connect('http://sotonwo.cloudapp.net:3005');
           if(data.geo != undefined){
             // resetAllKML();   
 
-            var lat =   data.geo.coordinates[0];
-            var lng =   data.geo.coordinates[1];
+            var lat_fnd =   data.geo.coordinates[0];
+            var lng_fnd =   data.geo.coordinates[1];
+
+
+            var dataPoint = { 
+  					lat: lat_fnd, // x coordinate of the datapoint, a number 
+  					lng: lng_fnd, // y coordinate of the datapoint, a number
+  					count: data.id // the value at datapoint(x, y)
+			};
+
+            heatmapLayer.addData(dataPoint);
 
               //console.log(lat,lng)
-            var tweetLocation = new google.maps.LatLng(lat, lng);
+           //var tweetLocation = new google.maps.LatLng(lat, lng);
 
               // if((lat > lat1) && (lat < lat2)){
 
               //     if((lng > lon1) && (lng < lon2)){
 
-                      liveTweetsPos.push({location: tweetLocation, weight: 15});
+                      //////liveTweetsPos.push({location: tweetLocation, weight: 15});
                   //       try{
                   //   if(isInfoWindowOpen(infoWindow)){
                   //     infoWindow.close()
